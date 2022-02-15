90f7d542000f0d8bcabda199bbfb95d8fe97f13e
Ararat <16293599+mathdev112@users.noreply.github.com>
2022-02-15 16:11:30 +0400

    Unused variable removed

--- functions/src/configs.ts
+++ functions/src/configs.ts
@@ -4,7 +4,6 @@ const envValues = Object.keys(functions.config()).length ? functions.config() : 
 export const configs = {
   // Infura RPC provder and private key of cloud signer
   provider: envValues.env.rpc_provider,
-  signerPrivateKey: envValues.env.signer_private_key,
 
   // Biconomy values
   biconomyApiKey: envValues.env.biconomy_api_key,
